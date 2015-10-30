f3727e4245a9a406498cbf8082e03fd62677d01f
Nisarg <nisarg.gandhi64@gmail.com>
2015-10-30 01:50:16 -0400

    Sentiment Analysis using word2vec first draft

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Sentiment-Analysis-Word2Vec
+## Sentiment-Analysis-Word2Vec
+
+## Task
+I have used Word2Vec for sentiment analysis by attempting to classify the Cornell IMDB movie review corpus.
+
+## Dataset
+- Cornell IMDB movie review corpus (http://www.cs.cornell.edu/people/pabo/movie-review-data/).
+
+## References
+- Doc2vec: https://radimrehurek.com/gensim/models/doc2vec.html
+- Paper: http://arxiv.org/abs/1405.4053
---
+++ doc2vec_sentiment.py
@@ -0,0 +1,41 @@
+from gensim.models import Doc2Vec
+import numpy
+from sklearn.linear_model import LogisticRegression
+
+model = Doc2Vec.load('./imdb.d2v')
+
+train_arrays = numpy.zeros((25000, 100))
+train_labels = numpy.zeros(25000)
+
+for i in range(12500):
+    prefix_train_pos = 'TRAIN_POS_' + str(i)
+    prefix_train_neg = 'TRAIN_NEG_' + str(i)
+    train_arrays[i] = model[prefix_train_pos]
+    train_arrays[12500 + i] = model[prefix_train_neg]
+    train_labels[i] = 1
+    train_labels[12500 + i] = 0
+
+
+test_arrays = numpy.zeros((25000, 100))
+test_labels = numpy.zeros(25000)
+
+for i in range(12500):
+    prefix_test_pos = 'TEST_POS_' + str(i)
+    prefix_test_neg = 'TEST_NEG_' + str(i)
+    test_arrays[i] = model[prefix_test_pos]
+    test_arrays[12500 + i] = model[prefix_test_neg]
+    test_labels[i] = 1
+    test_labels[12500 + i] = 0
+
+
+classifier = LogisticRegression()
+classifier.fit(train_arrays, train_labels)
+
+
+
+
+LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
+          intercept_scaling=1, penalty='l2', random_state=None, tol=0.0001)
+
+
+classifier.score(test_arrays, test_labels)
